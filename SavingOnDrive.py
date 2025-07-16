06b94ddbd52ebfaa2c833f581d2b2e7413020e19
Test90-kw <t90852231@gmail.com>
2025-07-16 03:11:49 +0300

    Update SavingOnDrive.py

--- SavingOnDrive.py
+++ SavingOnDrive.py
@@ -10,7 +10,7 @@ class SavingOnDrive:
         self.credentials_dict = credentials_dict
         self.scopes = ['https://www.googleapis.com/auth/drive']
         self.service = None
-        self.parent_folder_id = '1VEmDWkK8VzdOqM0r4ldZ7fQeqoAcWrjq'  # Your parent folder ID
+        self.parent_folder_id = '1NqWSVrV95XdnCbZ5MCqVR-4O2JxCF3Up'  # Your parent folder ID
 
     def authenticate(self):
         """Authenticate with Google Drive API."""
