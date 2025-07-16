81224a0953f289b1b923b6b1407c9c310c2be592
Test90-kw <t90852231@gmail.com>
2025-07-16 03:11:17 +0300

    Update hierarchial_code_main.py

--- hierarchial_code_main.py
+++ hierarchial_code_main.py
@@ -20,7 +20,7 @@ class HierarchialMainScraper:
         self.credentials_dict = credentials_dict
         self.scopes = ['https://www.googleapis.com/auth/drive']
         self.service = None
-        self.parent_folder_id = '1HkfBH929hdehxGiDcthfq4Cf_hXKwN9T'
+        self.parent_folder_id = '1NqWSVrV95XdnCbZ5MCqVR-4O2JxCF3Up'
         
         # Scraping initialization
         self.url = url
